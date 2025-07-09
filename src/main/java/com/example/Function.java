2c0b23c69af7f32526d66fcf839f48316bbc4656
Prajna Prakash Shetty <prajnap@devtools.in>
2025-07-09 17:05:16 +0530

    Update Function.java

--- src/main/java/com/example/Function.java
+++ src/main/java/com/example/Function.java
@@ -1,19 +1,26 @@
 package com.example;
 
-import com.microsoft.azure.functions.*;
 import com.microsoft.azure.functions.annotation.*;
+import com.microsoft.azure.functions.*;
+
+import java.util.Optional;
 
 public class Function {
-    @FunctionName("hello")
+    @FunctionName("HttpExample")
     public HttpResponseMessage run(
-        @HttpTrigger(name = "req", methods = {HttpMethod.GET}, authLevel = AuthorizationLevel.ANONYMOUS)
+        @HttpTrigger(name = "req", methods = {HttpMethod.GET, HttpMethod.POST}, authLevel = AuthorizationLevel.ANONYMOUS)
         HttpRequestMessage<Optional<String>> request,
         final ExecutionContext context) {
 
-        String name = request.getQueryParameters().getOrDefault("name", "World");
-        return request
-                .createResponseBuilder(HttpStatus.OK)
-                .body("Hello, " + name)
-                .build();
+        context.getLogger().info("Java HTTP trigger processed a request.");
+
+        String name = request.getQueryParameters().get("name");
+        if (name == null) {
+            name = request.getBody().orElse("world");
+        }
+
+        return request.createResponseBuilder(HttpStatus.OK)
+            .body("Hello, " + name)
+            .build();
     }
 }
