9562e5932d83d39cc89d5e46b023ccb98335b719
Prajna Prakash Shetty <prajnap@devtools.in>
2025-07-10 10:25:34 +0530

    Update Function.java

--- src/main/java/com/example/Function.java
+++ src/main/java/com/example/Function.java
@@ -8,19 +8,15 @@ import java.util.Optional;
 public class Function {
     @FunctionName("HttpExample")
     public HttpResponseMessage run(
-        @HttpTrigger(name = "req", methods = {HttpMethod.GET, HttpMethod.POST}, authLevel = AuthorizationLevel.ANONYMOUS)
-        HttpRequestMessage<Optional<String>> request,
-        final ExecutionContext context) {
-
+        @HttpTrigger(
+            name = "req",
+            methods = {HttpMethod.GET},
+            authLevel = AuthorizationLevel.ANONYMOUS,
+            route = ""
+        ) HttpRequestMessage<Optional<String>> request,
+        final ExecutionContext context
+    ) {
         context.getLogger().info("Java HTTP trigger processed a request.");
-
-        String name = request.getQueryParameters().get("name");
-        if (name == null) {
-            name = request.getBody().orElse("world");
-        }
-
-        return request.createResponseBuilder(HttpStatus.OK)
-            .body("Hello, " + name)
-            .build();
+        return request.createResponseBuilder(HttpStatus.OK).body("Welcome to the homepage!").build();
     }
 }
