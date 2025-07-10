2be5139748bacb1c7e304528e4474533fa6b0e2a
Prajna Prakash Shetty <prajnap@devtools.in>
2025-07-10 18:57:20 +0530

    Update Function.java

--- src/main/java/com/example/Function.java
+++ src/main/java/com/example/Function.java
@@ -10,7 +10,7 @@ import java.util.Optional;
  */
 public class Function {
 
-    @FunctionName("HttpExample")
+    @FunctionName("rootHandler")
     public HttpResponseMessage run(
         @HttpTrigger(
             name = "req",
